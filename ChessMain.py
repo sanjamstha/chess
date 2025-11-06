391dca3b15551f5f7a038459452dd20d1c89789a
Sanjam Shrestha <shresthasanjam@gmail.com>
2025-11-06 20:00:46 +0545

    movable pieces - no rules

--- ChessMain.py
+++ ChessMain.py
@@ -30,10 +30,29 @@ def main():
     gs = ChessEngine.GameState()
     loadImages() #only do this once before the while loop
     running = True
+    sqSelected = () # no square selected initially, keep track of last click of the user (tuple: row, col)
+    playerClicks = [] # keep track of player clicks (two tuplesK: [(6,4),(4,4)])
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos() #x,y location of mouse
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col): # user selected same square twice
+                    sqSelected = () # deselect
+                    playerClicks = [] # clear player clicks
+                else:
+                    sqSelected = (row,col)
+                    playerClicks.append(sqSelected) # append for both 1st and 2nd clicks
+                if len(playerClicks) == 2: #after 2nd click
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = () # reset user clicks
+                    playerClicks = []
+
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
