df30dc4d632db4ad28cf16becdf5b09d06cdec86
Sanjam Shrestha <shresthasanjam@gmail.com>
2025-11-15 23:50:24 +0545

    pawn moves

--- ChessEngine.py
+++ ChessEngine.py
@@ -15,6 +15,8 @@ class GameState():
             ["wp","wp","wp","wp","wp","wp","wp","wp"],
             ["wR","wN","wB","wQ","wK","wB","wN","wR"]
         ]
+        self.moveFunctions = {'p': self.getPawnMoves, 'R': self.getRookMoves, 'N': self.getKnightMoves,
+                                'B': self.getBishopMoves, 'Q': self.getQueenMoves, 'K': self.getKingMoves}
         self.whiteToMove = True
         self.moveLog = []
     # Takes a move as parameter and executes it ( doesnt work for castling, en-passant, and pawn promotion )
@@ -38,26 +40,47 @@ class GameState():
 
     # All moves without considering checks
     def getAllPossibleMoves(self):
-        moves = [Move((6,4),(4,4), self.board)]
+        moves = []
         for r in range(len(self.board)):     # number of rows 
             for c in range(len(self.board[r])): # number of cols in given row
                 turn = self.board[r][c][0]
-                if (turn == 'w' and self.whiteToMove) and (turn == 'b' and not self.whiteToMove):
+                if (turn == 'w' and self.whiteToMove) or (turn == 'b' and not self.whiteToMove):
                     piece = self.board[r][c][1]
-                    if piece == 'p':
-                        self.getPawnMoves(r,c, moves)
-                    elif piece == 'r':
-                        self.getRookMoves(r,c,moves)
+                    self.moveFunctions[piece](r,c,moves) # calls the appropriate move functions based on the piece type
+                    # if piece == 'p':
+                    #     self.getPawnMoves(r,c, moves)
+                    # elif piece == 'R':
+                    #     self.getRookMoves(r,c,moves)
         return moves
                     
     # Get all the pawn moves for the pawn located at rol, col and add these moves to the list 
     def getPawnMoves(self, r, c, moves):
-        pass
+        if self.whiteToMove: #white pawn moves
+            if self.board[r-1][c] == "--": #1 sqaure pawn advance
+                moves.append(Move((r,c), (r-1,c), self.board))
+                if r == 6 and self.board[r-2][c] == "--":
+                    moves.append(Move((r,c),(r-2,c), self.board))
+            if c-1 >= 0: # captures to the left
+                if self.board[r-1][c-1][0] == 'b': #enemy piece to capture
+                    moves .append(Move((r,c),(r-1,c-1), self.board))
 
-    
-    # Get all the rook moves for the rook located at rol, col and add these moves to the list 
-    def getRookMoves(self, r, c, moves):
-        pass
+            if c+1 <= 7: # capture to the right
+                if self.board[r-1][c+1][0] == 'b': # enemy piece to capture
+                    moves.append(Move((r, c), (r-1, c+1), self.board))
+
+        else:   # black pawn moves
+            if self.board[r+1][c] == "--": # 1sq pawn advance
+                moves.append(Move((r,c),(r+1,c),self.board))
+                if r == 1 and self.board[r+2][c] == "--": # 2sq pawn advance
+                    moves.append(Move((r,c),(r+2,c),self.board))
+
+            if c+1 <= 7: # captures to the left 
+                if self.board[r+1][c+1][0] == 'w': #enemy piece to capture
+                    moves.append(Move((r,c),(r+1,c+1), self.board))
+
+            if c-1 >= 0: # capture to the right
+                if self.board[r+1][c-1][0] == 'w': # enemy piece to capture
+                    moves.append(Move((r, c), (r+1, c-1), self.board))
 
 
 
@@ -79,7 +102,7 @@ class Move():
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
         self.moveID = self.startRow *1000 + self.startCol *100 + self.endRow*10 + self.endCol
-        print(self.moveID)
+        # print(self.moveID)
 
     # Overriding the equals method
     def __eq__(self, other):
