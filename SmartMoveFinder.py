66a3087274c65fc525556631a4c6f44049e8a496
Sanjam Shrestha <shresthasanjam@gmail.com>
2025-11-20 22:41:17 +0545

    minmax algo, no recursion

--- SmartMoveFinder.py
+++ SmartMoveFinder.py
@@ -4,27 +4,53 @@ pieceScore = {"K":100, "Q":9, "R":5, "B":3, "N":3, "p":1}
 CHECKMATE = 1000
 STALEMATE = 0
 
-
 def findRandomMove(validMoves):
     return validMoves[random.randint(0, len(validMoves)-1)]
 
+# Greedy ALgorithm
+# def findBestMove(gs, validMoves):
+#     turnMultiplier = 1 if gs.whiteToMove else -1
+#     maxScore = -CHECKMATE
+#     bestMove = None
+#     for playerMove in validMoves:
+#         gs.makeMove(playerMove)
+#         if gs.checkMate:
+#             score = -CHECKMATE
+#         elif gs.staleMate:
+#             score = STALEMATE
+#         else:
+#             score = turnMultiplier * scoreMaterial(gs.board)
+#         if score > maxScore:
+#             maxScore = score
+#             bestMove = playerMove
+#         gs.undoMove()
+#     return bestMove
+
+# MinMax Algorithm with no recursion 
 def findBestMove(gs, validMoves):
     turnMultiplier = 1 if gs.whiteToMove else -1
-    maxScore = -CHECKMATE
-    bestMove = None
+    opponentMinMaxScore = CHECKMATE
+    bestPlayerMove = None
     for playerMove in validMoves:
         gs.makeMove(playerMove)
-        if gs.checkMate:
-            score = -CHECKMATE
-        elif gs.staleMate:
-            score = STALEMATE
-        else:
-            score = turnMultiplier * scoreMaterial(gs.board)
-        if score > maxScore:
-            maxScore = score
-            bestMove = playerMove
+        opponentsMoves = gs.getValidMoves()
+        opponentMaxScore = -CHECKMATE
+        for opponentsMove in opponentsMoves:
+            gs.makeMove(opponentsMove)
+            if gs.checkMate:
+                score = -turnMultiplier * CHECKMATE
+            elif gs.staleMate:
+                score = STALEMATE
+            else:
+                score = -turnMultiplier * scoreMaterial(gs.board)
+            if score > opponentMaxScore:
+                opponentMaxScore = score
+            gs.undoMove()
+        if opponentMinMaxScore > opponentMaxScore:
+            opponentMinMaxScore = opponentMinMaxScore
+            bestPlayerMove = playerMove
         gs.undoMove()
-    return bestMove
+    return bestPlayerMove
 
 
 '''
